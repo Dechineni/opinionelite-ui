6535c018eeb4a488edbbb037e57543bce8c3beb6
Aakash Patil <aakash@quantifyai.co>
2026-07-01 16:42:34 +0530

    Added Alert message comment to the propject report

--- src/app/(app)/projects/projectdetail/ProjectReportPanel.tsx
+++ src/app/(app)/projects/projectdetail/ProjectReportPanel.tsx
@@ -190,6 +190,7 @@ export default function ProjectReportPanel({
           : "PreScreen Report preview will appear here"}
       </div>
 
+      {/* Alert Message for Downloading Report */}
       {alertMessage && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40">
           <div className="w-[340px] rounded-lg bg-white p-6 text-center shadow-lg">
